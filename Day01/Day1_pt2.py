2cd393aafec6632702a00b15475a646f88e93339
Evan Kessel <the.Admiral.Miner@gmail.com>
2020-11-30 22:46:29 -0700

    Reading
    Made the program read from a .txt file

--- Day01/Day1_pt2.py
+++ Day01/Day1_pt2.py
@@ -4,21 +4,24 @@ Created on Mon Nov 30 22:10:54 2020
 
 @author: hidej
 """
-inp = input("Enter Input\n")
-expenses = inp.split()
-
-for index in range(len(expenses)):
-    expenses[index] = int(expenses[index])
-result = 0   
-for num1 in expenses:
-    for num2 in expenses:
-        for num3 in expenses:
-            if num1 + num2 + num3 == 2020:
-                result = num1 * num2 * num3
+# inp = input("Enter Input\n")
+with open("input.txt") as file:
+    inp = file.read()
+    
+    expenses = inp.split()
+    
+    for index in range(len(expenses)):
+        expenses[index] = int(expenses[index])
+    result = 0   
+    for num1 in expenses:
+        for num2 in expenses:
+            for num3 in expenses:
+                if num1 + num2 + num3 == 2020:
+                    result = num1 * num2 * num3
+                    break
+            if result != 0:
                 break
         if result != 0:
             break
-    if result != 0:
-        break
-
-print(result)
+    
+    print(result)
